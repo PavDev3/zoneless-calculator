da527a024d6e109f454e5dc929290804d3936b25
pablo <pnunfe@gmail.com>
2024-10-31 11:41:01 +0100

    initial commit for initial project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@/calculator/views/calculator-view/calculator-view.component'),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calculator-view.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class CalculatorViewComponent {}
