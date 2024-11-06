cb0686e62e8cb59ce04b410b94b4138828f20883
pablo <pnunfe@gmail.com>
2024-11-06 14:36:55 +0100

    double button

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -3,9 +3,9 @@ import {
   ChangeDetectionStrategy,
   Component,
   ElementRef,
-  HostBinding,
   input,
   output,
+  signal,
   viewChild,
 } from '@angular/core';
 
@@ -17,20 +17,20 @@ import {
   styleUrls: ['./calculator-button.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
   },
 })
 export class CalculatorButtonComponent {
+  public isPressed = signal(false);
+
   public isCommand = input(false, { transform: booleanAttribute });
   public isDoubleSize = input(false, { transform: booleanAttribute });
   public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
   public onClick = output<string>();
 
   public value = input<string>();
-
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
-  }
 
   handleClick() {
     if (!this.contentValue()?.nativeElement) {
