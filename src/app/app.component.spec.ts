f6d69107c32816fa153da77384bedcd1ede7cd10
pablo <pnunfe@gmail.com>
2024-11-13 11:44:15 +0100

    new test for elements

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -37,4 +37,23 @@ describe('AppComponent', () => {
   it('should render RouterOutlet', () => {
     expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
+
+  it('should render router-outlet wrapped with css class', () => {
+    const divElement = compiled.querySelector('div');
+    const cssClass =
+      'min-w-screen min-h-screen bg-slate-600 flex items-center justify-center px-5 py-5'.split(
+        ' '
+      );
+    expect(divElement).not.toBeNull();
+    divElement?.classList.forEach((className) => {
+      expect(cssClass).toContain(className);
+    });
+  });
+
+  it("should contain the 'buy me a beer' link ", () => {
+    const linkElement = compiled.querySelector('a');
+    expect(linkElement).not.toBeNull();
+    expect(linkElement?.title).toEqual('Buy me a beer');
+    expect(linkElement?.href).toContain('https://www.buymeacoffee.com/');
+  });
 });
