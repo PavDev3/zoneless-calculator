b8148b9bd9e3861c4a18169c480da012b4121857
pablo <pnunfe@gmail.com>
2024-11-14 12:17:00 +0100

    test calculator view component

--- src/app/calculator/views/calculator-view/calculator-view.component.spec.ts
+++ src/app/calculator/views/calculator-view/calculator-view.component.spec.ts
@@ -17,6 +17,22 @@ describe('CalculatorViewComponent', () => {
   });
 
   it('should create the component', () => {
+    console.log(compiled);
     expect(component).toBeTruthy();
+  });
+
+  it('should contain calculator component', () => {
+    expect(compiled.querySelector('calculator')).not.toBeNull();
+  });
+
+  it('should contain basic css classes', () => {
+    const divElement = compiled.querySelector('div');
+    const shouldHave =
+      'w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden'.split(
+        ' '
+      );
+    shouldHave.forEach((className) => {
+      expect(divElement?.classList.contains(className)).toBeTruthy();
+    });
   });
 });
